473f48e481719f8f4ca2497a435e4edeaf3b9681
jeonghoHub <tjsrsdqq4@gmail.com>
2022-09-14 01:36:28 +0900

    feat: typescript 문자열 계산기 레포 추가

--- string-calculator-kotlin/src/main/kotlin/StringCalculator.kt
+++ string-calculator-kotlin/src/main/kotlin/StringCalculator.kt
@@ -1,19 +1,21 @@
-//import com.sun.org.apache.xalan.internal.xsltc.runtime.Operators
-//import java.util.LinkedList
-//import java.util.Queue
-//
-//class StringCalculator() {
-//    fun calculate(expressionParam: Vaildator): Double {
-//        val operators = CalculationMaterialExtractor.operatorExtraction(expressionParam.get())
-//        val operands = CalculationMaterialExtractor.operandExtraction(expressionParam.get())
-////        return calculate(operators, operands)
-//    }
-//
-////    fun calculate(operators: Queue<Char>, operands: Queue<Double>): Double {
-////
-////        return 40.0
-////    }
-//
-//
-//}
-//
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Operators
+import java.util.LinkedList
+import java.util.Queue
+
+class StringCalculator() {
+    fun calculate(expressionParam: Vaildator): Double {
+        val operators = CalculationMaterialExtractor.operatorExtraction(expressionParam.get())
+        val operands = CalculationMaterialExtractor.operandExtraction(expressionParam.get())
+        return calculate(operators, operands)
+    }
+
+    private fun calculate(operators: Queue<Char>, operands: Queue<Double>): Double {
+        var result: Double = operands.poll()
+        while(!operators.isEmpty()) {
+            result = Operator.findByOperator(operators.poll()).apply(result, operands.poll())
+        }
+        return result
+    }
+
+}
+
