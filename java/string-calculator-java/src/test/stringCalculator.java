426fa9c028241c321eec4b7e971a1bfb87340235
usa_develop <tjsrsdqq4@gmail.com>
2024-12-17 16:23:36 +0900

    :recycle: 폴더정리

--- java/string-calculator-java/src/test/stringCalculator.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class stringCalculator {
-    @Test
-    void 계산식이_들어오면_계산한다() {
-        String number = "2 + 3 + 6";
-        int number = 236;
-    }
-}
